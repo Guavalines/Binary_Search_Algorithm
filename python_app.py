4dd58b6eaa4001d9ccfa0bef60ec4a51f709b4de
guavalines <konstrictor@gmail.com>
2023-05-02 23:32:42 -0700

    finish binary search

--- python_app.py
+++ python_app.py
@@ -7,3 +7,21 @@ def binary_search(list, element):
 
     while(start<=end):
         print("Step",steps, ":", str(list[start:end + 1]))
+
+        steps = steps+1
+        middle = (start + end) // 2
+
+        if element == list[middle]:
+            return middle
+        if element < list[middle]:
+            end = middle -1
+        else:
+            start = middle + 1
+
+    return -1
+
+
+my_list = [1,2,3,4,5,6,7,8,9]
+target = 2
+
+binary_search(my_list, target)
